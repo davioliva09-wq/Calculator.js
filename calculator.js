7e0fc936ca977046e7844f402835374cc4c5c903
Davi Oliva <seuemail@gmail.com>
2026-03-15 14:26:15 -0300

    comentarios explicativos

--- calculator.js
+++ calculator.js
@@ -3,15 +3,23 @@ let display = document.getElementById("display");
 let currentinput ="";
 let currentOperator="";
 
+//textContent propriedade DOM
+
+
+
+//esta função pega o novo numero adicionado
 function appendnumber(value){
 currentinput +=value
 display.textContent = currentinput
 }
 
+/*se curretinput for vazio e operador diferente de "."" nao retorna NADA*/
 
+//o parametro operador pegará o operador digitado na calculadora curretinput = ''+operador = operador digitado
 function pegaope(operador){
     if(currentinput==="" && operador !== ".") return''
     currentinput += operador;
+    //textContent  ganha o valor de curretinput
     display.textContent = currentinput;
 }
 
@@ -20,12 +28,15 @@ function calculate(){
     try{ 
 
 let result = eval(currentinput);
-
+//verifica se o resultado é DIFERENTE de um numero inteiro
 if(!Number.isInteger(result))
+
+    //caso seja um numero decimal, limita a 2 casas decimais
 result = result.toFixed(2)
 
 currentinput = result
 display.textContent = currentinput;
+
     } catch(error){
 display.textContent = "erro";
 currentinput ="";
